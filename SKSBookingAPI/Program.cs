f634971d3096d391f63b73434ab982d50c0bf709
Freja Pedersen <tokeahp@gmail.com>
2024-08-12 13:35:31 +0200

    facepalm

--- SKSBookingAPI/Program.cs
+++ SKSBookingAPI/Program.cs
@@ -27,10 +27,15 @@ namespace SKSBookingAPI
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
+            app.UseSwagger();
+            app.UseSwaggerUI();
+
+            /*
             if (app.Environment.IsDevelopment()) {
                 app.UseSwagger();
                 app.UseSwaggerUI();
             }
+            */
 
             app.UseHttpsRedirection();
 
