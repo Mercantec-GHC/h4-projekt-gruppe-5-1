28698bd57f2c7aafd07b9d1ae09c90090406b13d
Freja Pedersen <tokeahp@gmail.com>
2024-08-12 12:11:23 +0200

    Ahoi

---
+++ SKSBookingAPI/Context/AppDBContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SKSBookingAPI.Models;
+
+namespace SKSBookingAPI.Context
+{
+    public class AppDBContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        public AppDBContext(DbContextOptions<AppDBContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ SKSBookingAPI/Models/Common.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SKSBookingAPI.Models {
+    public class Common {
+        [Key]
+        public int ID { get; set; } // Kan erstattes med "int Id"
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
