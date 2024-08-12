774f972ab9b0a4a29ad2bb46fb55ae098a8c2a1d
Freja Pedersen <tokeahp@gmail.com>
2024-08-12 12:36:25 +0200

    Fikset

---
+++ SKSBookingAPI/Controllers/StatusController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SKSBookingAPI.Context;
+
+namespace SKSBookingAPI.Controllers {
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StatusController : ControllerBase {
+        private readonly AppDBContext _context;
+
+        public StatusController(AppDBContext context) {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult GetStatus() {
+            return Ok("The server is live!");
+        }
+
+        [HttpGet("DB")]
+        public IActionResult GetStatusDB() {
+            if (_context.Database.CanConnect()) {
+                return Ok("The database and server are live!");
+            }
+            else return NotFound();
+        }
+    }
+}
--- SKSBookingAPI/Program.cs
+++ SKSBookingAPI/Program.cs
@@ -10,14 +10,6 @@ namespace SKSBookingAPI
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
-
-            builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
-
-            var app = builder.Build();
-
             IConfiguration Configuration = builder.Configuration;
 
             string connectionString = Configuration.GetConnectionString("DefaultConnection")
@@ -26,6 +18,13 @@ namespace SKSBookingAPI
             builder.Services.AddDbContext<AppDBContext>(options => {
                 options.UseNpgsql(connectionString);
             });
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
 
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment()) {
