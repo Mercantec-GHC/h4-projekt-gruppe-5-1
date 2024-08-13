5721779ac1798b091e3b67cf49701bcabe732ab1
Kjetil <Kjetil_T@hotmail.com>
2024-08-13 10:53:57 +0200

    User DTO's

--- SKSBookingAPI/Models/User.cs
+++ SKSBookingAPI/Models/User.cs
@@ -9,3 +9,20 @@
         // Only for educational purposes, not in the final product!
     }
 }
+
+public class UserDTO {
+    public string Id {get; set; }
+    public string Email {get; set; }
+    public straing Username {get; set; }
+}
+
+public class LoginDTO {
+    public string Email {get; set; }
+    public string Password {get; set; }
+}
+
+public class SignUpDTO {
+    public string Email {get; set; }
+    public string Username {get; set; }
+    public string Password {get; set; }
+}
