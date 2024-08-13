deb17ee263171e5822f8e2c5f3ace1fba99a0606
Freja Pedersen <tokeahp@gmail.com>
2024-08-13 11:31:30 +0200

    User opdateret

--- SKSBookingAPI/Models/User.cs
+++ SKSBookingAPI/Models/User.cs
@@ -1,7 +1,8 @@
 ﻿namespace SKSBookingAPI.Models {
-    public class User : Common {
+    public class User : Common { // Indsæt billede!
         public required string Email { get; set; }
         public required string Username { get; set; }
+        public required string PhoneNumber { get; set; }
         public required string HashedPassword { get; set; }
         public required string Salt { get; set; }
         public DateTime LastLogin { get; set; }
@@ -11,18 +12,20 @@
 }
 
 public class UserDTO {
-    public string Id {get; set; }
-    public string Email {get; set; }
-    public straing Username {get; set; }
+    public int ID { get; set; }
+    public string Email { get; set; }
+    public string Username { get; set; }
+    public string PhoneNumber { get; set; }
 }
 
 public class LoginDTO {
-    public string Email {get; set; }
-    public string Password {get; set; }
+    public string Email { get; set; }
+    public string Password { get; set; }
 }
 
 public class SignUpDTO {
-    public string Email {get; set; }
-    public string Username {get; set; }
-    public string Password {get; set; }
+    public string Email { get; set; }
+    public string Username { get; set; }
+    public string Password { get; set; }
+    public string PhoneNumber { get; set; }
 }
