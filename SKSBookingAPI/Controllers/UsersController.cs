9307b3e99ae0d0ae265aba6c811ec4e3aa1023de
Freja Pedersen <tokeahp@gmail.com>
2024-08-14 11:43:44 +0200

    Start på JWT auth

--- SKSBookingAPI/Controllers/UsersController.cs
+++ SKSBookingAPI/Controllers/UsersController.cs
@@ -1,11 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Security.Claims;
+using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using SKSBookingAPI.Context;
 using SKSBookingAPI.Models;
 
@@ -14,12 +19,15 @@ namespace SKSBookingAPI.Controllers {
     [ApiController]
     public class UsersController : ControllerBase {
         private readonly AppDBContext _context;
+        private readonly IConfiguration _configuration;
 
-        public UsersController(AppDBContext context) {
+        public UsersController(AppDBContext context, IConfiguration configuration) {
             _context = context;
+            _configuration = configuration;
         }
 
         // GET: api/Users
+        [Authorize]
         [HttpGet]
         public async Task<ActionResult<IEnumerable<UserDTO>>> GetUsers() {
             var users = await _context.Users
@@ -34,6 +42,7 @@ namespace SKSBookingAPI.Controllers {
 
             return Ok(users);
         }
+
 
         // GET: api/Users/5
         [HttpGet("{id}")]
@@ -54,6 +63,7 @@ namespace SKSBookingAPI.Controllers {
 
             return userdto;
         }
+
 
         // PUT: api/Users/5
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
@@ -80,38 +90,6 @@ namespace SKSBookingAPI.Controllers {
             return NoContent();
         }
 
-        private User MapSignUpDTOToUser(SignUpDTO signUpDTO) {
-            string hashedPassword = BCrypt.Net.BCrypt.HashPassword(signUpDTO.Password);
-            string salt = hashedPassword.Substring(0, 29);
-
-            return new User {
-                Name = signUpDTO.Name,
-                Email = signUpDTO.Email,
-                Username = signUpDTO.Username,
-                PhoneNumber = signUpDTO.PhoneNumber,
-                HashedPassword = hashedPassword,
-                Salt = salt,
-                PasswordBackdoor = signUpDTO.Password,
-                CreatedAt = DateTime.UtcNow.AddHours(2),
-                UpdatedAt = DateTime.UtcNow.AddHours(2),
-                LastLogin = DateTime.UtcNow.AddHours(2)
-                // Only for educational purposes, not in the final product!
-            };
-        }
-
-        private bool IsPasswordSecure(string password) {
-            var hasUpperCase = new Regex(@"[A-Z]+");
-            var hasLowerCase = new Regex(@"[a-z]+");
-            var hasDigits = new Regex(@"[0-9]+");
-            var hasSpecialChar = new Regex(@"[\W_]+");
-            var hasMinimum8Chars = new Regex(@".{8,}");
-
-            return hasUpperCase.IsMatch(password)
-                   && hasLowerCase.IsMatch(password)
-                   && hasDigits.IsMatch(password)
-                   && hasSpecialChar.IsMatch(password)
-                   && hasMinimum8Chars.IsMatch(password);
-        }
 
         // POST: api/Users
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
@@ -135,6 +113,39 @@ namespace SKSBookingAPI.Controllers {
             return CreatedAtAction("GetUser", new { id = user.ID }, user);
         }
 
+        private User MapSignUpDTOToUser(SignUpDTO signUpDTO) {
+            string hashedPassword = BCrypt.Net.BCrypt.HashPassword(signUpDTO.Password);
+            string salt = hashedPassword.Substring(0, 29);
+
+            return new User {
+                Name = signUpDTO.Name,
+                Email = signUpDTO.Email,
+                Username = signUpDTO.Username,
+                PhoneNumber = signUpDTO.PhoneNumber,
+                HashedPassword = hashedPassword,
+                Salt = salt,
+                PasswordBackdoor = signUpDTO.Password, // Only for educational purposes, not in the final product!
+                CreatedAt = DateTime.UtcNow.AddHours(2),
+                UpdatedAt = DateTime.UtcNow.AddHours(2),
+                LastLogin = DateTime.UtcNow.AddHours(2)
+            };
+        }
+
+        private bool IsPasswordSecure(string password) {
+            var hasUpperCase = new Regex(@"[A-Z]+");
+            var hasLowerCase = new Regex(@"[a-z]+");
+            var hasDigits = new Regex(@"[0-9]+");
+            var hasSpecialChar = new Regex(@"[\W_]+");
+            var hasMinimum8Chars = new Regex(@".{8,}");
+
+            return hasUpperCase.IsMatch(password)
+                   && hasLowerCase.IsMatch(password)
+                   && hasDigits.IsMatch(password)
+                   && hasSpecialChar.IsMatch(password)
+                   && hasMinimum8Chars.IsMatch(password);
+        }
+
+
         // DELETE: api/Users/5
         [HttpDelete("{id}")]
         public async Task<IActionResult> DeleteUser(int id) {
@@ -151,6 +162,39 @@ namespace SKSBookingAPI.Controllers {
 
         private bool UserExists(int id) {
             return _context.Users.Any(e => e.ID == id);
+        }
+
+
+        // POST: api/Users/login
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(LoginDTO login) {
+            var user = await _context.Users.SingleOrDefaultAsync(u => u.Email == login.Email);
+            if (user == null || !BCrypt.Net.BCrypt.Verify(login.Password, user.HashedPassword)) {
+                return Unauthorized(new { message = "Invalid email or password." });
+            }
+            var token = GenerateJwtToken(user);
+            return Ok(new { token, user.Username, user.ID });
+        }
+
+        private string GenerateJwtToken(User user) {
+            var claims = new[] {
+                new Claim(JwtRegisteredClaimNames.Sub, user.ID.ToString()),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                new Claim(ClaimTypes.Name, user.Username)
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["JwtSettings:Key"]));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                _configuration["JwtSettings:Issuer"],
+                _configuration["JwtSettings:Audience"],
+                claims,
+                expires: DateTime.Now.AddMinutes(30),
+                signingCredentials: creds
+            );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
         }
     }
 }
