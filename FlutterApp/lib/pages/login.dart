1c23c80a4314608b899936b7ae0c1e2b3fbc0505
Christian Hammervig <christian@hammervig.dk>
2024-08-23 11:24:13 +0200

    login tilføjet med billede

--- FlutterApp/lib/pages/login.dart
+++ FlutterApp/lib/pages/login.dart
@@ -1,66 +1,100 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-
+import 'auth_state.dart';
 import '../main.dart';
 
+class LoginPage extends StatefulWidget {
+  const LoginPage({Key? key, required void Function() onCreateUser})
+      : super(key: key);
 
-class LoginPage extends StatelessWidget {
+  @override
+  State<LoginPage> createState() => _LoginState();
+}
+
+class _LoginState extends State<LoginPage> {
+  Map userData = {};
   final TextEditingController emailController = TextEditingController();
   final TextEditingController passwordController = TextEditingController();
-  final VoidCallback onCreateUser;
-
-  LoginPage({required this.onCreateUser});
+  late final VoidCallback onCreateUser;
 
   @override
   Widget build(BuildContext context) {
-    var appState = context.watch<MyAppState>();
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Login'),
+          centerTitle: true,
+        ),
+        body: SingleChildScrollView(
+            child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.only(top: 30.0),
+                child: Center(
+                  child: SizedBox(
+                    width: 120,
+                    height: 120,
+                    child: Image.asset('assets/logo.png'),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(20),
+                child: TextFormField(
+                  controller: emailController,
+                  decoration: InputDecoration(
+                    floatingLabelStyle: Theme.of(context).textTheme.titleLarge,
+                    border: OutlineInputBorder(),
+                    labelText: 'Email',
+                  ),
+                ),
+              ),
+              Padding(
+                  padding: const EdgeInsets.all(20),
+                  child: TextFormField(
+                    controller: passwordController,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Password',
+                    ),
+                    obscureText: true,
+                  )),
+              Padding(
+                padding: const EdgeInsets.all(20),
+                child: ElevatedButton.icon(
+                  onPressed: () async {
+                    final authState =
+                        Provider.of<AuthState>(context, listen: false);
+                    await authState.logIn(
+                        emailController.text, passwordController.text);
 
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(20),
-            child: TextFormField(
-              controller: emailController,
-              decoration: InputDecoration(
-                floatingLabelStyle: Theme.of(context).textTheme.titleLarge,
-                border: OutlineInputBorder(),
-                labelText: 'Email',
+                    if (authState.isLoggedIn) {
+                      // Navigate to another page
+                      Navigator.pushReplacement(
+                        context,
+                        MaterialPageRoute(builder: (context) => MyHomePage()),
+                      );
+                    } else {
+                      // Show error message or handle login failure
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(
+                            content: Text('Login failed. Please try again.')),
+                      );
+                    }
+                  },
+                  icon: Icon(Icons.login),
+                  label: Text('Login'),
+                ),
               ),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(20), 
-            child: TextFormField(
-              controller: passwordController,
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: 'Password',
-              ),
-              obscureText: true,
-            )
-          ),
-          Padding(
-            padding: const EdgeInsets.all(20), 
-            child: ElevatedButton.icon(
-                onPressed: () async {
-                  await appState.login(emailController.text, passwordController.text);
-                },
-                icon: Icon(Icons.login),
-                label: Text('Login'),
-              ),
-          ),
-          ElevatedButton.icon(
+              ElevatedButton.icon(
                 onPressed: onCreateUser,
                 icon: Icon(Icons.create),
                 label: Text('Create User'),
               ),
-        ],
-      ),
-
-    );
-
+            ],
+          ),
+        )));
   }
 }
